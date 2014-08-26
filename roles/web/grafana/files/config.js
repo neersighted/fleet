50d69a8eb368821c8eaa9cedc8b3edced1765008
Bjorn Neergaard <bjorn@neersighted.com>
2014-08-26 07:12:16 -0700

    Broken graphite-api instead of graphite-ng

--- roles/web/grafana/files/config.js
+++ roles/web/grafana/files/config.js
@@ -2,35 +2,25 @@ define(['settings'],
 function (Settings) {
   return new Settings({
     datasources: {
-      influxdb: {
-        type: 'influxdb',
-        url: "http://localhost:8086/db/metrics",
-        username: 'root',
-        password: 'root',
+      graphite: {
+        type: 'graphite',
+        url: "http://localhost:8080",
       },
-      grafana: {
-        type: 'influxdb',
-        url: "http://localhost:8086/db/grafana",
-        username: 'root',
-        password: 'root',
-        grafanaDB: true
-      },
+      elasticsearch: {
+        type: 'elasticsearch',
+        url: "http://localhost:9200",
+        index: 'grafana',
+        grafanaDB: true,
+      }
     },
 
     default_route: '/dashboard/file/default.json',
     unsaved_changes_warning: true,
 
-    search: {
-      max_results: 20
-    },
     playlist_timespan: "1m",
+    search: { max_results: 20 },
 
-    admin: {
-      password: ''
-    },
-
-    plugins: {
-      panels: []
-    }
+    admin: { password: '' },
+    plugins: { panels: [] }
   });
 });
